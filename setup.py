d271e47bff2abf00f27587c71571e2688cfc1d14
ShaMal <malhotrashaurya11@gmail.com>
2026-03-11 00:08:30 +0530

    fixed author name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Shaurya Malhotra",
+    author_email="malhotrashaurya11@gmail.com",
     packages=find_packages()
 )
 
